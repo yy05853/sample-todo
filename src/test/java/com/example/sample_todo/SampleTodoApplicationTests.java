f013a5c7713ab91ce925668c9db111ff06d8676c
c05853yy <c05853yy@penguin>
2024-10-04 18:26:54 +0900

    初回コミット

---
+++ src/test/java/com/example/sample_todo/SampleTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
