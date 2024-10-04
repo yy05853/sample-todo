27e0690609e64b8b45ba274cfabd72e70afd6814
c05853yy <c05853yy@penguin>
2024-10-04 21:43:56 +0900

    テーブルからデータを取得する

---
+++ src/main/java/com/example/sample_todo/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.example.sample_todo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.sample_todo.entity.Task;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+
+    Task findById(long id);
+    
+}
