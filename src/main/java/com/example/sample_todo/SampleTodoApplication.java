adec6b44df8da09fd4d121911d4e5c9d80e2ca19
c05853yy <c05853yy@penguin>
2024-11-02 10:15:56 +0900

    Add comment

--- src/main/java/com/example/sample_todo/SampleTodoApplication.java
+++ src/main/java/com/example/sample_todo/SampleTodoApplication.java
@@ -3,6 +3,10 @@ package com.example.sample_todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// このクラスはSpring Initializrで勝手に作成されるが、重要なので消してはいけない。
+// このクラスがある階層以下のクラスはSpring Bootのコンポーネントスキャンの対象となる。
+// 簡単に言うと、このクラスがないとSpring Bootが起動しないし、@Controllerや@Serviceなどのアノテーションの意味がなくなる。
+// https://zenn.dev/simsim/articles/52a65b7189cfe2#1.-%40springbootapplication
 @SpringBootApplication
 public class SampleTodoApplication {
 
--- src/main/java/com/example/sample_todo/repository/TaskRepository.java
+++ src/main/java/com/example/sample_todo/repository/TaskRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.sample_todo.entity.Task;
 
 @Repository
+// これはインターフェース。CrudRepositoryを継承するだけでDB操作が可能になる
+// https://qiita.com/shukawam/items/6e379df031dccebddd36#repository-%E3%82%A4%E3%83%B3%E3%82%BF%E3%83%95%E3%82%A7%E3%83%BC%E3%82%B9%E3%82%92%E5%AE%9A%E7%BE%A9%E3%81%99%E3%82%8B
 public interface TaskRepository extends CrudRepository<Task, Long> {}
